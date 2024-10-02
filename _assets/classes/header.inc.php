b14e0782c44dbe354deef739d1594dfaffd18883
irving <irving.rmz00@gmail.com>
2024-10-02 17:11:45 -0600

    agregando venders

--- _assets/classes/header.inc.php
+++ _assets/classes/header.inc.php
@@ -25,6 +25,7 @@ header("Cache-control: private");
     require('usuarios.class.php');
     require('products.class.php');
     require('compradores.class.php');
+    require('venders.class.php');
 
 
     $MySqlHandler =  MySqlPdoHandler::getInstance(); 
@@ -35,6 +36,7 @@ header("Cache-control: private");
     $Usuarios      = new Usuarios($MySqlHandler/*, $secure->getCurrentUser()*/);
     $Productos      = new Products($MySqlHandler/*, $secure->getCurrentUser()*/);
     $compradores      = new compradores($MySqlHandler/*, $secure->getCurrentUser()*/);
+    $venders      = new Usuarios($MySqlHandler/*, $secure->getCurrentUser()*/);
 
 
     /*if($get_user = $secure->getCurrentUser()){
