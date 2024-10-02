e1329c33593b965daf2e7368b883d1857d43fc67
Danrmz7 <irving.rmz00@gmail.com>
2024-10-02 15:55:32 -0600

    clase de venders creada

---
+++ _assets/classes/venders.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File Name: Market
+ * Creator URI: 
+ * Description: Main configuration file
+ * Author: 
+ * Version: 1.1
+ * Author URI: 
+ */
+class Venders {
+ 
+    var $sql;
+    var $mail;
+    var $current_user;
+    var $output;
+    var $alert;
+}
+
+?>
